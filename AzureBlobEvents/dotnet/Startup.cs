1121693f1023166af3593d8e28580446a6d31d38
Matt Cavey <48829594+MattCavey@users.noreply.github.com>
2023-12-13 13:26:06 +0000

    Adding missing files

---
+++ AzureBlobEvents/dotnet/Startup.cs
@@ -0,0 +1,18 @@
+﻿
+using CdrAzureFunctions.Interfaces;
+using CdrAzureFunctions.Services;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CdrAzureFunctions
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddSingleton<IBlobClientFactory, BlobClientFactory>();
+            builder.Services.AddTransient<IWriteFile, FileBlobWriter>();
+            builder.Services.AddTransient<IProtectFile, FileBlobProtectService>();
+        }
+    }
+}
